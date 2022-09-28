74620d0d62a61af2452e45cf5f811dc745ba4b77
Shahlo <shahlosoatova01@gmail.com>
2022-09-29 00:10:10 +0500

    create project

---
+++ js/main copy.js
@@ -0,0 +1,37 @@
+$('.slider-blog__inner').slick(
+    {
+        dots:true,
+        prevArrow: ' <button type="button" class="slick-prev"><img src="./img/portfolio/arrow-left.svg" alt="arrow-left"></button>',
+        nextArrow: ' <button type="button" class="slick-next"><img src="./img/portfolio/arrow-right.svg" alt="arrow-right"></button>'
+        
+    }
+);
+
+$(function(){
+    $('.slider-blog__inner').slick(
+        {
+            dots:true,
+            prevArrow: ' <button type="button" class="slick-prev"><img src="./img/portfolio/arrow-left.svg" alt="arrow-left"></button>',
+            nextArrow: ' <button type="button" class="slick-next"><img src="./img/portfolio/arrow-right.svg" alt="arrow-right"></button>'
+            
+        }
+    )
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var mixer = mixitup('.portfolio__content');
